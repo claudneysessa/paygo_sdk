c4e88e66f5fe0ebc07e9dd05d7a922baebd873e0
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2023-03-11 22:02:54 -0300

    Ajuste o retorno do intent no exemplo do SDK

--- example/lib/presentation/application/application_controller.dart
+++ example/lib/presentation/application/application_controller.dart
@@ -1,7 +1,82 @@
+import 'dart:async';
+import 'package:receive_intent/receive_intent.dart' as receive_intent;
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/paygo_integrado.dart';
 
 class ApplicationController extends GetxController {
+  late StreamSubscription _subscription;
+
+  get subscription => _subscription;
+
+  Future<void> _initReceiveIntent() async {
+    _subscription = receive_intent.ReceiveIntent.receivedIntentStream.listen(
+      (
+        receive_intent.Intent? intent,
+      ) {
+        if (intent?.data != null) {
+          final Uri uri = Uri.parse(intent?.data ?? '');
+
+          final String decodedUri = Uri.decodeFull(
+            uri.toString(),
+          );
+
+          TransacaoRequisicaoResposta? resposta;
+          resposta = TransacaoRequisicaoResposta.fromUri(
+            decodedUri,
+          );
+
+          Get.defaultDialog(
+            titlePadding: const EdgeInsets.all(2),
+            contentPadding: const EdgeInsets.all(20),
+            title: "Resposta do PayGo Integrado",
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Operation',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  resposta.operation,
+                ),
+                const Text(
+                  'ID',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  resposta.transactionId,
+                ),
+                const Text(
+                  'Mensagem',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  resposta.resultMessage,
+                ),
+              ],
+            ),
+          );
+
+          loggerNoStack.i(
+            "Intent Data: ${resposta.toMap()}",
+          );
+        }
+      },
+      onError: (err) {
+        loggerNoStack.e("Erro ao receber Intent");
+      },
+    );
+  }
+
   var logger = Logger(
     printer: PrettyPrinter(),
   );
@@ -9,4 +84,16 @@ class ApplicationController extends GetxController {
   var loggerNoStack = Logger(
     printer: PrettyPrinter(methodCount: 0),
   );
+
+  @override
+  void onInit() async {
+    await _initReceiveIntent();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    _subscription.cancel();
+    super.onClose();
+  }
 }
--- example/lib/presentation/views/home_controller.dart
+++ example/lib/presentation/views/home_controller.dart
@@ -1,54 +1,14 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/types/card_type.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/types/fin_type.dart';
-import 'package:receive_intent/receive_intent.dart' as receive_intent;
 
 import 'package:paygo_sdk/paygo_sdk.dart';
 import 'package:paygo_sdk_example/presentation/application/application_controller.dart';
 
 class HomeController extends GetxController {
-  late StreamSubscription _subscription;
   ApplicationController applicationController = Get.find<ApplicationController>();
-
-  get subscription => _subscription;
-
-  Future<void> _initReceiveIntent() async {
-    _subscription = receive_intent.ReceiveIntent.receivedIntentStream.listen(
-      (
-        receive_intent.Intent? intent,
-      ) {
-        if (intent?.data != null) {
-          final Uri uri = Uri.parse(intent?.data ?? '');
-
-          final String decodedUri = Uri.decodeFull(
-            uri.toString(),
-          );
-
-          TransacaoRequisicaoResposta? resposta;
-          resposta = TransacaoRequisicaoResposta.fromUri(
-            decodedUri,
-          );
-
-          Get.defaultDialog(
-            title: "Resposta",
-            content: Text(
-              resposta.resultMessage,
-            ),
-          );
-
-          applicationController.loggerNoStack.i(
-            "Intent Data: ${resposta.toMap()}",
-          );
-        }
-      },
-      onError: (err) {
-        applicationController.loggerNoStack.e("Erro ao receber Intent");
-      },
-    );
-  }
 
   Future<void> homeStatusChannelClick() async {
     PayGOSdk repository = PayGOSdk();
@@ -125,17 +85,5 @@ class HomeController extends GetxController {
         ..cardType = CardType.cartaoDebito
         ..finType = FinType.aVista,
     );
-  }
-
-  @override
-  void onInit() async {
-    await _initReceiveIntent();
-    super.onInit();
-  }
-
-  @override
-  void onClose() {
-    _subscription.cancel();
-    super.onClose();
   }
 }
